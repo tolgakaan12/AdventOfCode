40de14fc620070fcbf0464598c1c6510875df41c
tolgakaan12 <celebitolgakaan@gmail.com>
2025-12-02 22:10:54 +0000

    day 1 puzzle 1 refactor

--- 2025/Day_1/Puzzle_1/solution.cpp
+++ 2025/Day_1/Puzzle_1/solution.cpp
@@ -6,26 +6,7 @@
 
 using namespace std;
 
-vector<string> split_line(const string& str, char delimiter) {
-
-  vector<string> tokens;
-  string token;
-  istringstream tokenStream(str);
-
-  while (getline(tokenStream, token, delimiter)) {
-    tokens.push_back(token);
-  }
-  return tokens;
-}
-
-
 int main() {
-
-  int count = 0;
-
-  int modulo = 100;
-
-  int dial = 50;
 
   ifstream input_file("input.txt");
 
@@ -34,47 +15,28 @@ int main() {
     return 1;
   }
 
+  int count = 0;
+  int modulo = 100;
+  int dial = 50;
+
   string line;
-
-  int line_count = 0;
-
   while (getline(input_file, line)) {
-    ++line_count;
-    cout << "processed line: " << line_count << "\n";
+      if (line.empty()) continue;
 
-    vector<string> entries = split_line(line, '\n');
+      char direction_char = line[0];
+      int direction_int = (direction_char == 'R') ? +1: -1;
 
-
-    for (const string& entry: entries) {
-
-      char direction_char = entry[0];
-      int direction_int = 0;
-
-      if (direction_char == 'R') {
-        direction_int = +1;
-      }
-      else {
-        direction_int = -1;
-      }
-
-      string magnitude_str = entry.substr(1);
-
+      string magnitude_str = line.substr(1);
       int magnitude_int = stoi(magnitude_str);
 
       int current = direction_int * magnitude_int;
-
       dial += current;
-
       dial = (dial + modulo) % modulo;
 
-      cout << dial << "\n";
+      // cout << dial << "\n";
 
-      if (dial == 0) {
-        ++count;
-      }
-      
+      if (dial == 0) ++count;
     }
-  }
   cout << "Count: " << count << "\n";
-  return count;
+  return 0;
 }
